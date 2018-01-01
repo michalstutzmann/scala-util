fb84b4e3d400cc8605c8413d4a358a885da20698
Michał Węgrzyn <michal.wegrzyn@gmail.com>
2018-01-01 17:55:11 +0100

    Bugfixes

--- src/main/scala/com/github/mwegrz/scalautil/avro4s/codecs.scala
+++ src/main/scala/com/github/mwegrz/scalautil/avro4s/codecs.scala
@@ -1,13 +1,25 @@
 package com.github.mwegrz.scalautil.avro4s
 
+import java.nio.ByteBuffer
 import java.time.Instant
 
 import com.sksamuel.avro4s._
-import org.apache.avro.{ Schema, SchemaBuilder }
+import org.apache.avro.{ Conversions, LogicalTypes, Schema, SchemaBuilder }
 import org.apache.avro.Schema.Field
 import scodec.bits.ByteVector
 
 object codecs {
+  /* Fixes https://github.com/sksamuel/avro4s/issues/141 */
+  implicit def BigDecimalToValue(implicit sp: ScaleAndPrecision = ScaleAndPrecision(2, 8)): ToValue[BigDecimal] = {
+    val decimalConversion = new Conversions.DecimalConversion
+    val decimalType = LogicalTypes.decimal(sp.precision, sp.scale)
+    new ToValue[BigDecimal] {
+      override def apply(value: BigDecimal): ByteBuffer = {
+        decimalConversion.toBytes(value.setScale(sp.scale).bigDecimal, null, decimalType)
+      }
+    }
+  }
+
   implicit val ByteToSchema: ToSchema[Byte] = new ToSchema[Byte] {
     protected val schema = Schema.create(Schema.Type.INT)
   }
