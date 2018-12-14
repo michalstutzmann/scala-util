6c85c9e9d5ddc9a6c05f3e9c28a4e5269a0889ba
Michał Węgrzyn <michal.wegrzyn@gmail.com>
2018-12-14 13:39:32 +0100

    Cosmetics

--- src/main/scala/com/github/mwegrz/scalautil/avro4s/coding.scala
+++ src/main/scala/com/github/mwegrz/scalautil/avro4s/coding.scala
@@ -30,80 +30,11 @@ object coding {
       generic.value.from(value :: HNil)
     }
 
-  /*implicit def anyValValueClassSchemaFor[ValueClass, Ref, Value <: AnyVal](
-      implicit generic: Lazy[Generic.Aux[ValueClass, Ref]],
-      evidence: Ref <:< (Value :: HNil),
-      schemaFor: SchemaFor[Value]): SchemaFor[ValueClass] =
-    SchemaFor.const(schemaFor.schema)
-
-  implicit def anyValValueClassEncoder[ValueClass, Ref, Value <: AnyVal](
-      implicit generic: Lazy[Generic.Aux[ValueClass, Ref]],
-      evidence: Ref <:< (Value :: HNil),
-      encoder: Encoder[Value]): Encoder[ValueClass] =
-    encoder.comap(generic.value.to(_).head)
-
-  implicit def anyValValueClassDecoder[ValueClass, Ref, Value <: AnyVal](
-      implicit generic: Lazy[Generic.Aux[ValueClass, Ref]],
-      evidence: (Value :: HNil) =:= Ref,
-      decoder: Decoder[Value]): Decoder[ValueClass] =
-    decoder.map { value =>
-      generic.value.from(value :: HNil)
-    }
-
-  implicit def stringValueClassSchemaFor[ValueClass, Ref](
-      implicit generic: Lazy[Generic.Aux[ValueClass, Ref]],
-      evidence: Ref <:< (String :: HNil),
-      schemaFor: SchemaFor[String]): SchemaFor[ValueClass] = SchemaFor.const(schemaFor.schema)
-
-  implicit def stringValueClassEncoder[ValueClass, Ref](
-      implicit generic: Lazy[Generic.Aux[ValueClass, Ref]],
-      evidence: Ref <:< (String :: HNil),
-      encoder: Encoder[String]): Encoder[ValueClass] = encoder.comap(generic.value.to(_).head)
-
-  implicit def stringValueClassDecoder[ValueClass, Ref](
-      implicit generic: Lazy[Generic.Aux[ValueClass, Ref]],
-      evidence: (String :: HNil) =:= Ref,
-      decoder: Decoder[String]): Decoder[ValueClass] = decoder.map { value =>
-    generic.value.from(value :: HNil)
-  }
-
-  implicit def bigDecimalValueClassSchemaFor[ValueClass, Ref](
-      implicit generic: Lazy[Generic.Aux[ValueClass, Ref]],
-      evidence: Ref <:< (BigDecimal :: HNil),
-      schemaFor: SchemaFor[BigDecimal]): SchemaFor[ValueClass] = SchemaFor.const(schemaFor.schema)
-
-  implicit def bigDecimalValueClassEncoder[ValueClass, Ref](
-      implicit generic: Lazy[Generic.Aux[ValueClass, Ref]],
-      evidence: Ref <:< (BigDecimal :: HNil),
-      encoder: Encoder[BigDecimal]): Encoder[ValueClass] = encoder.comap(generic.value.to(_).head)
-
-  implicit def bigDecimalValueClassDecoder[ValueClass, Ref](
-      implicit generic: Lazy[Generic.Aux[ValueClass, Ref]],
-      evidence: (BigDecimal :: HNil) =:= Ref,
-      decoder: Decoder[BigDecimal]): Decoder[ValueClass] = decoder.map { value =>
-    generic.value.from(value :: HNil)
-  }*/
-
   implicit val ByteVectorSchemaFor: SchemaFor[ByteVector] =
     SchemaFor.const(Schema.create(Schema.Type.BYTES))
   implicit val ByteVectorEncoder: Encoder[ByteVector] =
     Encoder.ByteVectorEncoder.comap(_.toArray.toVector)
   implicit val ByteVectorDecoder: Decoder[ByteVector] = Decoder.ByteVectorDecoder.map(ByteVector(_))
-
-  /*implicit def byteVectorValueClassSchemaFor[ValueClass, Ref](
-      implicit generic: Lazy[Generic.Aux[ValueClass, Ref]],
-      evidence: Ref <:< (ByteVector :: HNil)): SchemaFor[ValueClass] =
-    valueClassSchemaFor[ValueClass, Ref, ByteVector]
-
-  implicit def byteVectorValueClassEncoder[ValueClass, Ref](
-      implicit generic: Lazy[Generic.Aux[ValueClass, Ref]],
-      evidence: Ref <:< (ByteVector :: HNil)): Encoder[ValueClass] =
-    valueClassEncoder[ValueClass, Ref, ByteVector]
-
-  implicit def byteVectorValueClassDecoder[ValueClass, Ref](
-      implicit g: Lazy[Generic.Aux[ValueClass, Ref]],
-      evidence: (ByteVector :: HNil) =:= Ref): Decoder[ValueClass] =
-    valueClassDecoder[ValueClass, Ref, ByteVector]*/
 
   implicit val DurationSchemaFor: SchemaFor[Duration] =
     SchemaFor.const(Schema.create(Schema.Type.STRING))
