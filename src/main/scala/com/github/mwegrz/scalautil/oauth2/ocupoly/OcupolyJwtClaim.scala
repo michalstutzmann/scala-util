00b000c4ea7178bc455936cec03eed4c59922f1a
Michał Stutzmann <michal.stutzmann@ocupoly.com>
2020-06-14 20:40:03 +0200

    Cosmetics

--- src/main/scala/com/github/mwegrz/scalautil/oauth2/ocupoly/OcupolyJwtClaim.scala
+++ src/main/scala/com/github/mwegrz/scalautil/oauth2/ocupoly/OcupolyJwtClaim.scala
@@ -35,12 +35,11 @@ object OcupolyJwtClaim {
     } else {
       Set.empty[String]
     }
-    OcupolyJwtClaim("", sub, aud, iat, exp, scope, organization)
+    OcupolyJwtClaim(sub, aud, iat, exp, scope, organization)
   }
 }
 
 final case class OcupolyJwtClaim(
-    iss: String = "",
     sub: String,
     aud: Set[String],
     iat: Long,
