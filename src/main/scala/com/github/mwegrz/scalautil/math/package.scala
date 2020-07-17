1beeb9d91d2ebf8869c7258484356607a1af8865
Michał Stutzmann <michal.stutzmann@ocupoly.com>
2020-07-17 06:42:52 +0200

    Fixes

--- src/main/scala/com/github/mwegrz/scalautil/math/package.scala
+++ src/main/scala/com/github/mwegrz/scalautil/math/package.scala
@@ -6,4 +6,17 @@ package object math {
 
   def round(value: BigDecimal, decimalPlaces: Int): Double =
     value.setScale(decimalPlaces, BigDecimal.RoundingMode.HALF_UP).toDouble
+
+  def normalize(value: Double, min: Double, max: Double): Double = {
+    require(max > min, "max must be greater than min")
+    require(value <= max && value >= min, "value must be between min and max")
+
+    (value - min) / (max - min)
+  }
+  def denormalize(value: Double, min: Double, max: Double): Double = {
+    require(max > min, "max must be greater than min")
+    require(value >= 0 && value <= 1, "value must be between 0 and 1")
+
+    (value * (max - min) + min)
+  }
 }
